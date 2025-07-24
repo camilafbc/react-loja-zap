abfce2b3b2d29fc45fb5dce7f2b34da1a51b5c2e
Camila F. B. Coelho <camilafernanda914@gmail.com>
2025-07-24 20:46:10 -0300

    style(home): Adiciona estilização para que o conteudo principal preencha todo o espaço possivel e mantenha o footer no fim da página

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <div className="w-full max-w-4xl mx-auto">
+    <div className="flex flex-col min-h-screen w-full max-w-4xl mx-auto">
       <Header />
-      <div className="mx-3">
+      <div className="mx-3 flex-grow">
         <Suspense fallback={<TabsSkeleton />}>
           <ProductsTab />
         </Suspense>
